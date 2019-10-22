d0d9dca016a914ae85308c093e788b2aab0efc5a
Antonella <divirgilio.antonella@gmail.com>
2019-10-22 14:09:15 -0300

    styles
    set general styles

--- src/components/ContentPanel/index.js
+++ src/components/ContentPanel/index.js
@@ -1,9 +1,15 @@
 import React, { useContext } from "react";
 import { LanguageContext } from "../../context/LanguageContext";
 
+// Languages JSON
 import en from "../../locales/en.json";
 import es from "../../locales/es.json";
 import ja from "../../locales/ja.json";
+
+// Languages images
+import jaFlag from "../../assets/images/japan.png";
+import enFlag from "../../assets/images/united-kingdom.png";
+import esFlag from "../../assets/images/spain.png";
 
 const ContentPanel = () => {
   let { currentLocale, setCurrentLocale } = useContext(LanguageContext);
@@ -14,10 +20,40 @@ const ContentPanel = () => {
 
   return (
     <>
-      {currentLocale.text}
-      <button onClick={handleLanguageChange(en)}>ingles</button>
-      <button onClick={handleLanguageChange(es)}>español</button>
-      <button onClick={handleLanguageChange(ja)}>japonés</button>
+      <div className="language-container">
+        <button onClick={handleLanguageChange(en)} className="flag-container">
+          <img src={enFlag} alt={"english language"} className="flag-image" />
+        </button>
+        <button onClick={handleLanguageChange(es)} className="flag-container">
+          <img src={esFlag} alt={"spanish language"} className="flag-image" />
+        </button>
+        <button onClick={handleLanguageChange(ja)} className="flag-container">
+          <img src={jaFlag} alt={"japanese language"} className="flag-image" />
+        </button>
+      </div>
+      <div className="content-container">{currentLocale.text}</div>
+      <style>{`
+        .language-container,
+        .content-container {
+          text-align: center;
+        }
+
+        .content-container {
+          margin: 40px 0 0;
+          font-size: 30px;
+          font-family: 'Kosugi Maru', sans-serif;
+        }
+
+        .flag-container {
+          background: none;
+          border: none;
+          cursor: pointer;
+        }
+
+        .flag-image {
+          width: 50px;
+        }
+      `}</style>
     </>
   );
 };
